65c0accf1462c1516b017eae003501def5430769
Rifki Er <rifkierlangga17@gmail.com>
2024-01-14 05:57:32 +0700

    feat: ticket slicing

--- frontend/src/app/register/layout.jsx
+++ frontend/src/app/register/layout.jsx
@@ -1,6 +1,17 @@
-export default function Layout({ children}) {
-    return (
-      children
-    )
-  }
-  
+export default function Layout({ children }) {
+  return (
+    <div className="relative flex min-h-[100vh] items-center">
+      <img
+        src="/image/background/bg-register.png"
+        className="absolute bottom-0 z-10"
+        alt=""
+      />
+      <img
+        src="/image/background/bg-register.png"
+        className="absolute right-0 top-0 z-10 rotate-180"
+        alt=""
+      />
+      {children}
+    </div>
+  );
+}
---
+++ frontend/src/app/register/ticket/page.jsx
@@ -0,0 +1,59 @@
+'use client';
+import Button from '@/common/components/Button/Button';
+import { useState } from 'react';
+import QRCode from 'react-qr-code';
+
+const TicketPage = ({}) => {
+  const [registerData, setRegisterData] = useState(
+    localStorage.getItem('register-data')
+      ? JSON.parse(localStorage.getItem('register-data'))
+      : {
+          name: 'Shaddam',
+          email: 'shaddam.ah@gmail.com',
+          phone_number: '081234567890',
+        }
+  );
+  return (
+    <>
+      <div className="container flex rounded-[20px] border border-orange-1 px-[70px] py-[32px]">
+        <div className="flex w-full flex-col">
+          <div className="flex w-full justify-between">
+            <div className="flex w-full flex-col lg:w-8/12 ">
+              <h1 className="text-[48px] font-semibold">
+                SAN Main Event E-Ticket
+              </h1>
+              <span className="text-[28px] font-semibold">
+                Pool Party, Workshop
+              </span>
+              <div className="mt-6  flex flex-col">
+                <div className="flex items-center gap-[20px] text-[22px] font-semibold">
+                  <span className="min-w-[190px]">Name</span>
+                  <span>{registerData.name}</span>
+                </div>
+                <div className="flex items-center gap-[20px] text-[22px] font-semibold">
+                  <span className="min-w-[190px]">Email</span>
+                  <span>{registerData.email}</span>
+                </div>
+                <div className="flex items-center gap-[20px] text-[22px] font-semibold">
+                  <span className="min-w-[190px]">Phone Number</span>
+                  <span>{registerData.phone_number} </span>
+                </div>
+              </div>
+            </div>
+            <div className="flex w-[320px] flex-col items-center rounded-[20px] border border-green-1 p-8">
+              <QRCode value={registerData.id} class />
+              <span className="mt-5 text-xl">
+                *Please show this ticket to staff in registry
+              </span>
+            </div>
+          </div>
+          <div className="mt-8 self-end">
+            <Button>Download</Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TicketPage;
