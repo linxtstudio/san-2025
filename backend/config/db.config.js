bab64caab4ff0ba6e92f60a62850169a3a52008d
IlhamShiddiq <shdqillham123@gmail.com>
2024-01-18 16:52:01 +0700

    feature: docker compose prod

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -18,11 +18,11 @@ module.exports = {
         dialect: '-',
     },
     production: {
-        username: '-',
-        password: '-',
-        database: '-',
-        host: '-',
-        port: '-',
-        dialect: '-',
+        username: process.env.DB_USERNAME || 'postgres',
+        password: process.env.DB_PASSWORD || 'postgres',
+        database: process.env.DB_NAME || 'san_2024',
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || '5432',
+        dialect: 'postgres',
     }
 };
