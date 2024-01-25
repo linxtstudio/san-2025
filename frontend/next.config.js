172773b4017a0d58c474fb78ca74d6917cb89224
Shaddam Amru Hasibuan <shaddam.a.h@gmail.com>
2024-01-25 10:46:16 +0700

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  images: {
+    domains: [
+      "st3.depositphotos.com",
+    ],
+  },
   eslint: {
     dirs: ['src'],
   },
