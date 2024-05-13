06201a972a90e793f5077d11181dcf51fc705b9b
unknown <shaddam.a.h@gmail.com>
2024-05-13 14:12:44 +0700

    feat: update to content

--- frontend/src/app/about/page.jsx
+++ frontend/src/app/about/page.jsx
@@ -17,31 +17,44 @@ export default function AboutPage() {
             <h1 className="text-3xl font-semibold leading-[48px] md:text-display">
               About Us
             </h1>
-            <div className="flex flex-col items-center gap-12 md:flex-row">
-              <div className="relative flex aspect-square w-full max-w-[400px]">
+            <div className="flex flex-col gap-12">
+              <div className="relative flex aspect-video w-full max-w-[800px]">
                 <Image
-                  src="/image/logo/san-2024.png"
+                  src="/image/about/san-about.png"
                   fill
                   alt=""
-                  className="flex aspect-square flex-shrink-0 object-contain"
+                  className="flex flex-shrink-0 object-contain"
                 />
               </div>
               <div className="flex max-w-screen-sm flex-col gap-8">
                 <p>
-                  Held in a different cities around Indonesia every year so the
-                  local community can experience being the host of the event and
-                  gain the exposure also recognition.
+                  Salsa Arisan Nyok (SAN) is Indonesia biggest and longest
+                  gathering event. It is started on 2015 based on 5 community
+                  movers initiative, Reyno, Alfa, Tono, Chandra & Beta.
+                </p>
+                <p>
+                  Salsa Arisan Nyok (SAN) being held in a different cities
+                  around Indonesia every year so the local community can
+                  experience being the host of the event and gain the exposure
+                  also recognition.
                 </p>
                 <p>
                   The committee are all dancers from the hosting city who are
                   willing to volunteer to create this unforgettable event
                 </p>
                 <p>
-                  2024 will be our 7th time doing the Salsa Arisan Nyok, and
-                  this time will be held in Bali. New ideas and more parties
-                  will be added this year in hope that everyone can enjoy this
-                  warm heartfelt event even more.
+                  This year, 2024 will be our 7th time doing the Salsa Arisan
+                  Nyok, and this time will be held in Bali. New ideas and more
+                  parties will be added this year in hope that everyone can
+                  enjoy this warm heartfelt event even more.
                 </p>
+                <p>
+                  Salsa Arisan Nyok is more than just non-profit gathering
+                  event, it is fully good faith from the heart of community to
+                  bring all social dancer together to celebrate and at the same
+                  time build a better sustainable social dance community.
+                </p>
+                <p>We&apos;ll see you in Bali!</p>
               </div>
             </div>
           </div>
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -49,7 +49,7 @@ const MAIN_EVENT_SCHEDULE = [
     description: '6 PM - 9 PM',
   },
   {
-    title: 'Final Bonie & Clyde',
+    title: 'Final Bonnie & Clyde',
     description: '6.30 PM',
   },
   {
@@ -107,7 +107,7 @@ export default function Home() {
         >
           <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />
         </svg>
-        <span className="text-green-7">Q&A</span>
+        <span className="text-green-7">B&C Registration</span>
       </Link>
       <section className="home-hero  flex flex-col items-start justify-center text-white">
         <div className="container">
@@ -207,6 +207,15 @@ export default function Home() {
                       title: 'Time',
                       description: '7 PM - 11 PM',
                     },
+                    {
+                      title: 'DJs',
+                      description: (
+                        <ul>
+                          <li>DJ Rezack</li>
+                          <li>DJ Billy</li>
+                        </ul>
+                      ),
+                    },
                   ]}
                 />
               </div>
@@ -267,10 +276,61 @@ export default function Home() {
                     },
                     {
                       title: 'DJs',
-                      description: 'To Be Announced',
+                      description: (
+                        <ul>
+                          <li>DJ Indra</li>
+                          <li>DJ Adhyn</li>
+                          <li>DJ Timoty (Competition)</li>
+                          <li>DJ Adish</li>
+                        </ul>
+                      ),
                     },
                   ]}
                 />
+              </div>
+            </div>
+            <div className="mt-12 flex flex-col gap-[90px] lg:flex-row">
+              <div className="flex w-full flex-col gap-8" id="prices">
+                <div className="flex flex-col">
+                  <h4 className="text-title-1 font-semibold">
+                    Bonnie & Clyde Competion
+                  </h4>
+                </div>
+                <ItemList
+                  items={[
+                    {
+                      title: 'Registration Fee',
+                      description: 'Person 75K - Couple 100K',
+                    },
+                    {
+                      title: 'Music',
+                      description: 'Salsa-Bachata-Kizomba',
+                    },
+                    {
+                      title: 'The Prizes',
+                      description: 'Trophy, Voucher',
+                    },
+                  ]}
+                />
+              </div>
+            </div>
+            <div className="mt-12 flex flex-col gap-[90px] lg:flex-row">
+              <div className="flex w-full flex-col gap-10">
+                <div className="flex flex-col">
+                  <h4 className="text-title-2 font-semibold">Rules:</h4>
+                </div>
+                <div className="flex w-full flex-col gap-10 lg:w-7/12">
+                  <ol className="list-decimal pl-4 text-lg">
+                    <li>Open Level</li>
+                    <li>Non-Dance Instructor & Non-Taxi Dancer</li>
+                    <li>Never compete as Professional</li>
+                    <li>Never win Jack & Jill in Salsa Arisan</li>
+                    <li>
+                      Already registered as Salsa Arisan 2024 Main Event
+                      Participant
+                    </li>
+                  </ol>
+                </div>
               </div>
             </div>
             <div className="mt-12 flex flex-col gap-[90px] lg:flex-row">
@@ -299,6 +359,15 @@ export default function Home() {
                     {
                       title: 'Time',
                       description: '10 PM - 1 AM',
+                    },
+                    {
+                      title: 'DJs',
+                      description: (
+                        <ul>
+                          <li>DJ Tysh</li>
+                          <li>DJ Alfa</li>
+                        </ul>
+                      ),
                     },
                   ]}
                 />
@@ -424,6 +493,15 @@ export default function Home() {
                       {
                         title: 'Time',
                         description: '10 PM - 2.30 AM',
+                      },
+                      {
+                        title: 'DJs',
+                        description: (
+                          <ul>
+                            <li>DJ Billy</li>
+                            <li>DJ Timoty</li>
+                          </ul>
+                        ),
                       },
                     ]}
                   />
