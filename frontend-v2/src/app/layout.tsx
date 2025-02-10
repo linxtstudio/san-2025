25703e54bdf6df0cf8d27aa61a2f04dbdc6608cc
Shaddam Amru Hasibuan <shaddam.a.h@gmail.com>
2025-02-10 16:53:37 +0700

    feat: init new revamp frontend project with nextjs 15 + typescript, tailwindcss 4

---
+++ frontend-v2/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-default",
+  weight: ["100", "300", "400", "500", "700", "900"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "SAN 2025 - The Most Anticipated Social Dance Gathering In Indonesia",
+  description:
+    "The highly awaited dance extravaganza that promises to be a spellbinding fusion of artistry, rhythm, and sheer spectacle of afro latin dances in Indonesia.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
